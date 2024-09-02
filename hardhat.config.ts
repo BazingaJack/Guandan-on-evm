6cb98f9dfb51c5e8f1dd99e11f5bf6ef13def3cd
bazingajack <bazingajack@163.com>
2024-09-03 01:33:01 +0800

    add holesky network rpc

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -20,6 +20,10 @@ const config: HardhatUserConfig = {
     sepolia: {
       url: "https://sepolia.infura.io/v3/753b2b9f9d4443bdb56aab7ae60bc8dc",
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    holesky: {
+      url: "https://holesky.infura.io/v3/753b2b9f9d4443bdb56aab7ae60bc8dc",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     }
   },
   paths: {
